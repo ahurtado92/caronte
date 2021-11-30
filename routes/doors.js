864fb96cd69b165f297f9ddb3da144f79a58184a
Antonio Hurtado <antoniohurtado92@gmail.com>
2021-11-30 14:46:45 +0100

    Commit

--- routes/doors.js
+++ routes/doors.js
@@ -33,22 +33,5 @@ router.get('/:host/:device/:tag', async(req, res) => {
     }
 });
 
-// Get con todos los documentos
-router.get('/doors', verificarAuth, async(req, res) => {
-  //const usuarioId = req.usuario._id;
-
-  try {
-    //const doorDb = await door.find({usuarioId});
-    const doorDb = await Device.find({});
-    res.json(groupDb);
-  } catch (error) {
-    return res.status(400).json({
-      mensaje: 'Ocurrio un error',
-      error
-    })
-  }
-});
-
-
 // Exportamos la configuración de express app
 module.exports = router;
