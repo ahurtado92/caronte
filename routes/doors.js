de0f7d7efc2f8e3b1549331928b046c99597ba4c
Antonio <antoniohurtado92@gmail.com>
2021-12-01 20:15:59 +0100

    Groups

--- routes/doors.js
+++ routes/doors.js
@@ -81,7 +81,7 @@ router.get('/doors', async(req, res) => {
     doorDb.forEach(async door=>{
       console.log(door.groups);
       console.log(groupDB);
-      let intersection = door.groups.filter(x => groupDB.includes(x));
+      let intersection = door.groups.filter(x => groupDB.includes(x._id));
       console.log(intersection);
     });
     res.json(doorDb);
